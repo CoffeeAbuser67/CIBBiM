27c6d1cab81f0524e3be0a0e196b39c5d97c7798
CoffeeAbuser67 <Hservers170224@gmail.com>
2024-11-19 23:18:46 -0300

    check point before changes

--- src/pages/labs/Infraestrutura.tsx
+++ src/pages/labs/Infraestrutura.tsx
@@ -124,6 +124,97 @@ const Card_Menu: React.FC = () => {
   );
 }; // . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
+// [●] Lab
+interface Lab {
+  Id: string;
+  LabName: string;
+  LabResponsible: string;
+  DescriptionComponent: () => JSX.Element;
+  EquipamentsQtd: number;
+  EquipamentsTitles: string[];
+  EquipamentsDesc: string[];
+  EquipamentsImages: string[];
+} // . . .
+
+// [✪] labsList
+const labsList: Lab[] = [
+  {
+    // [●] alimentos_bebidas
+    Id: "alimentos_bebidas",
+    LabName: "Laboratório de Alimentos e Bebidas Fermentadas",
+    LabResponsible: "Dra. Ana Paula Trovatti Uetanabaro",
+    DescriptionComponent: () => (
+      <>
+        <Text size="3" highContrast className=" whitespace-pre-wrap">
+          <Strong>Pesquisadora responsável:</Strong> Dra. Ana Paula Trovatti
+          Uetanabaro
+          {"\n"}
+          (aptuetanabaro@gmail.com)
+        </Text>
+
+        <Text size="3" highContrast className=" whitespace-pre-wrap">
+          <Strong>Grande área de atuação:</Strong> Projetos e pesquisa aplicados
+          em biotecnologia microbiana
+        </Text>
+
+        <Text size="3" highContrast className=" whitespace-pre-wrap">
+          <Strong>Expertise:</Strong> Isolamento e seleção de microrganismos de
+          fermentações naturais e artesanais para aplicações industriais:
+          Cerveja Artesanal; Cachaça Artesanal; Cafés especiais da Chapada da
+          Diamantina; Probióticos isolados da fermentação do cacau para uso
+          humano e animal; Kombucha; Cogumelos comestíveis.
+        </Text>
+
+        <Box className="flex flex-col gap-1">
+          <Strong> Expertises do grupo: </Strong>
+          <Text>
+            ● Coleta e isolamento de microrganismos em campo, especialmente de
+            diversas origens, como fermentação do cacau, cachaça artesanal,
+            kombucha, vinagre, endofíticos e cogumelos comestíveis.
+          </Text>
+
+          <Text>
+            ● Processamento de amostras, isolamento, cultivo, preservação,
+            caracterização e identificação fenotípica (fisiologia) e genotípica
+            (biomol) de leveduras e bactérias.
+          </Text>
+
+          <Text>
+            ● Utilização de resíduos agroindustriais para produção de enzimas de
+            interesse industrial.
+          </Text>
+
+          <Text>
+            ● Prospecção de enzimas microbianas de interesse biotecnológico.
+          </Text>
+          <Text>
+            ● Pesquisas para a seleção de microrganismos com potencial
+            biotecnológico para a produção/fermentação de: Cerveja, Cachaça,
+            Iogurte, Café e Kombucha.
+          </Text>
+          <Text>
+            ● Análises de resultados, preparo de relatório bem fundamentados,
+            escrita de projetos científicos nas áreas de nossa expertise.
+          </Text>
+        </Box>
+      </>
+    ),
+    EquipamentsQtd: 3,
+    EquipamentsTitles: [
+      "Cabine de Segurança Biológica (CBS) (1 unidade)",
+      "Ultrafreezer (1 unidade)",
+      "Ultracentrífuga de bancada (1 unidade)",
+    ],
+    EquipamentsDesc: [
+      "A Cabine de Segurança Biológica é um equipamento essencial para garantir a segurança em laboratórios que trabalham com agentes biológicos. Ela protege o usuário, o ambiente e o produto, contribuindo para a realização de trabalhos com segurança e confiabilidade. Na CSB Classe II são realizadas atividades de isolamento, repique de microrganismos e manipulação de reagentes.",
+      "São equipamentos essenciais em laboratórios, especialmente aqueles que trabalham com amostras biológicas sensíveis à temperatura. Eles são utilizados para armazenar materiais a temperaturas extremamente baixas, geralmente entre -40°C e -86°C. O ultrafreezer, mantido na temperatura de -86°C, é especialmente utilizado para a preservação de amostras celulares e de DNA. O principal objetivo do ultrafreezer é preservar a integridade e viabilidade de amostras biológicas por longos períodos.",
+      "A ultracentrífuga de bancada é um equipamento de laboratório de alta rotação, projetado para separar partículas muito pequenas em suspensão em um líquido com base em suas massas e densidades. Essas partículas podem ser desde moléculas grandes (como proteínas e ácidos nucleicos) até organelas celulares. Possuem uma ampla gama de aplicações em diversos campos da ciência, incluindo: Isolamento de ácidos nucleicos (DNA e RNA), purificação de proteínas, análise de sedimentação de partículas virais, fracionamento de organelas celulares (mitocôndrias, núcleos), estudo da interação entre moléculas, análise da densidade de gradiente para separar partículas subcelulares, purificação de fármacos e estudos de estabilidade.",
+    ],
+    EquipamentsImages: [FOTO1, FOTO2, FOTO3],
+  }, // . . .
+
+]; // . . . . . . .
+
 // <●> labHeadings
 const labHeadings = [
   "Laboratório de Alimentos e Bebidas Fermentadas",
@@ -174,7 +265,8 @@ const AlimentosEBebidas = () => {
         )}
       >
         <Box
-          // <●> contentCardAlimentosEBebidas . . . . . .
+          // . . .
+          // <●> contentCardAlimentosEBebidas
           id="contentCardAlimentosEBebidas"
           className="flex flex-col w-full gap-9"
         >
@@ -184,8 +276,8 @@ const AlimentosEBebidas = () => {
             <Heading size="5" className="mb-1" highContrast>
               {labHeadings[0]}
             </Heading>
-            <Heading size="4">{labHeadings[1]}</Heading>
-            <Heading size="4">{labHeadings[2]}</Heading>
+            <Heading size="4">Centro de Inovação em Biologia e Biotecnologia Microbiana</Heading>
+            <Heading size="4">Universidade Estadual de Santa Cruz, Ilhéus, Bahia, Brasil</Heading>
           </Dialog.Title>
 
           <Dialog.Description>
@@ -255,16 +347,16 @@ const AlimentosEBebidas = () => {
             </Text>
 
             <Box
+              // . . . . . . . . . . . . . .
               // HERE  GRID
               className="columns-2 md:columns-3 gap-4 items-center"
             >
               <Box
-                // . . . . . . . .
                 // _PIN_ MANSONRY CARD 1
                 id="MasonryCard1"
               >
                 <Card
-                  className="flex flex-col gap-8 shadow-md bg-slate-200"
+                  className="flex flex-col gap-5 shadow-md bg-slate-200"
                   size="2"
                   mb="6"
                 >
@@ -293,12 +385,12 @@ const AlimentosEBebidas = () => {
               </Box>
 
               <Box
-                // . . . . . . . .
+                // . . .
                 // _PIN_ MANSONRY CARD 2
                 id="MasonryCard2"
               >
                 <Card
-                  className="flex flex-col gap-8 shadow-md bg-slate-200"
+                  className="flex flex-col gap-5 shadow-md bg-slate-200"
                   size="2"
                   mb="6"
                 >
@@ -328,12 +420,12 @@ const AlimentosEBebidas = () => {
               </Box>
 
               <Box
-                // . . . . . . . .
+                // . . .
                 // _PIN_ MANSONRY CARD 3
                 id="MasonryCard3"
               >
                 <Card
-                  className="flex flex-col gap-8 shadow-md bg-slate-200"
+                  className="flex flex-col gap-5 shadow-md bg-slate-200"
                   size="2"
                   mb="6"
                 >
